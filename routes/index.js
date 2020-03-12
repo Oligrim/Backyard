13194e2d7e0dbe076fea7bf919ab2de706341cc1
Oligrim <61763736+Oligrim@users.noreply.github.com>
2020-03-12 11:30:48 -0300

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-
 router.get('/', function(req, res, next) {
-	//console.log(req.headers);
-  res.render('index', { title: 'Head printonizer', cabezales: JSON.stringify(req.headers) });
-});	
+  console.log(req.headers);
+  res.render('index', { title: 'Express', juan : JSON.stringify(req.headers) });
+  
+});
 
 module.exports = router;
